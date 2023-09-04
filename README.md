85470fbafb7208f60dffe46ef9e8505ca6eb5f47
misno <michael.snow@ingka.com>
2023-09-04 12:01:56 +0000

    [new] added model + extras

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# product-description-sl
-Streamlit app for product description
+# Product Description model
+
+![Example Image](logo.jpeg)
+
+## Description
+
+A model that generates a description or a classification of an image of a product.
+
+## Examples: 
+
+- **Description:** This is an image of a white wardrobe with 3 doors, 2 drawers, made by IKEA
+
+- **Classification:** This is not a wardrobe.
+
+- **Custom (e.g. what material is this product made of?:** This wardrobe is made out of wood.
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+import streamlit as st
+
+from PIL import Image
+from lavis.models import load_model_and_preprocess
+
+
+st.set_page_config(page_title="Product description", page_icon=":robot:", layout="centered")
+st.header("Product description")
+
+st.image(image='logo.jpeg', width=700)
+
+st.markdown("## Please upload your product image")
+
+upload = st.file_uploader("Upload your image")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    option_model = st.selectbox(
+        'Which model do you wish to use?',
+        ('pretrain_flant5xl', 'pretrain_flant5xl_vitL', 'pretrain_flant5xxl', 'caption_coco_flant5xl'))
+    
+with col2:
+    option_prompt = st.selectbox(
+        'Would you like a product description, a product classification, or a custom question?',
+        ('Description', 'Classification', 'Custom'))
+
+    
+if option_prompt == 'Custom':
+    def get_text():
+        """
+        :output: function to receive prompt from user
+        """
+        input_text = st.text_area(label="Question", label_visibility='collapsed', placeholder="What is your question about the image?...", key="text_input")
+        return input_text
+
+    if len(text_input.split(" ")) > 25:
+        st.write("Please enter a shorter question. The maximum length is 25 words.")
+        st.stop()
+
+    prompt = get_text()
+    
+if option_prompt == 'Description':
+    prompt = 'Can you describe this image?'
+
+if option_prompt == 'Classification':
+    prompt = 'Is this product a wardrobe?'
+    
+
+if upload and prompt:
+    image = Image.open(img_path).convert('RGB')
+    
+    st.image(image, width=700)
+    st.markdown('## Model loading')
+    model, vis_processors, _ = load_model_and_preprocess(name="blip2_t5", model_type=option_model, is_eval=True, device='cpu')
+    st.markdown('## Model loaded')
+    
+    # model.generate({"image": image, "prompt": "Can you give a detailed description of this image?"})
+    response = model.generate({"image": image, "prompt": "Is this a picture of a wardrobe?"})
+    
+    st.markdown("### Model response: + /n")
+    st.markdown(response)
+    
