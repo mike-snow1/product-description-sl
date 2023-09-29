4c76b2c784622e7e2538b9c1369710007932c64a
mike-snow1 <89791771+mike-snow1@users.noreply.github.com>
2023-09-29 07:33:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import cv2
+
 import streamlit as st
 
 from PIL import Image
-import cv2
-import numpy as np
-from collections import Counter
 
 
+def get_color_name(Red, Green, Blue):
+        minimum = 10000
+        for i in range(len(csv)):
+            d = abs(Red - int(csv.loc[i, "Red"])) + abs(Green - int(csv.loc[i, "Green"])) + abs(Blue - int(csv.loc[i, "Blue"]))
+            if d <= minimum:
+                minimum = d
+                cname = csv.loc[i, "Color"]
+        return cname
+    
 st.set_page_config(page_title="Product description", page_icon=":robot:", layout="centered")
 st.header("Product description")
 
@@ -14,70 +22,63 @@ st.image(image='logo.jpeg', width=700)
 st.markdown("## Please upload your product image")
 
 upload = st.file_uploader("Upload your image")
-
-# col1, col2 = st.columns(2)
-
-# with col1:
-#     option_model = st.selectbox(
-#         'Which model do you wish to use?',
-#         ('pretrain_flant5xl', 'pretrain_flant5xl_vitL', 'pretrain_flant5xxl', 'caption_coco_flant5xl'))
-    
-# with col2:
-#     option_prompt = st.selectbox(
-#         'Would you like a product description, a product classification, or a custom question?',
-#         ('Description', 'Classification', 'Custom'))
-
-    
-# if option_prompt == 'Custom':
-#     def get_text():
-#         """
-#         :output: function to receive prompt from user
-#         """
-#         input_text = st.text_area(label="Question", label_visibility='collapsed', placeholder="What is your question about the image?...", key="text_input")
-#         return input_text
-
-#     if len(text_input.split(" ")) > 25:
-#         st.write("Please enter a shorter question. The maximum length is 25 words.")
-#         st.stop()
-
-#     prompt = get_text()
-    
-# if option_prompt == 'Description':
-#     prompt = 'Can you describe this image?'
-
-# if option_prompt == 'Classification':
-#     prompt = 'Is this product a wardrobe?'
-    
-
-# if upload and prompt and option_model:
-#     image = Image.open(upload).convert('RGB')
-    
-#     st.image(image, width=700)
-#     st.markdown('## Model loading')
-#     model, vis_processors, _ = load_model_and_preprocess(name="blip2_t5", model_type=option_model, is_eval=True, device='cpu')
-#     st.markdown('## Model loaded')
-
-#     image = vis_processors["eval"](raw_image).unsqueeze(0).to('cpu')
-    
-#     # model.generate({"image": image, "prompt": "Can you give a detailed description of this image?"})
-#     response = model.generate({"image": image, "prompt": "Is this a picture of a wardrobe?"})
-    
-#     st.markdown("### Model response: + /n")
-#     st.markdown(response)
     
 if upload:
     image = Image.open(upload).convert('RGB')
-    st.image(image, width=700)
+    st.image(image, width=700)   
 
-    image = cv2.imread(upload)
+    # Load the image
+    img = Image.open(upload)
 
-    # Reshape the 3D image array into a 2D array where each row represents a pixel (R, G, B values)
-    pixels = image.reshape(-1, 3)
+    # Convert the image to RGB mode if it's not already
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
+    # Define a threshold for the white background (adjust as needed)
+    background_threshold = 253
+
+    # Initialize a dictionary to store unique RGB values of the object
+    object_colors = {}
+
+    # Get the image dimensions
+    width, height = img.size
+
+    # Iterate through each pixel in the image
+    for x in range(width):
+        for y in range(height):
+            # Get the RGB values of the pixel
+            r, g, b = img.getpixel((x, y))
+
+            # Check if the pixel is not part of the white background
+            if r < background_threshold and g < background_threshold and b < background_threshold:
+                # Store the RGB values in the dictionary
+                object_colors[(r, g, b)] = object_colors.get((r, g, b), 0) + 1
+                
+    colours = []
+    counts = []
+
+    for color, count in object_colors.items():
+        colours.append(get_color_name(color[0], color[1], color[2]))
+        counts.append(count)
+
+    df = pd.DataFrame()
+
+    df['colour'] = colours
+    df['value_count'] = counts
     
-    pixel_counts = Counter(map(tuple, pixels))
+    D = df.groupby('colour').sum()
+    D['ratio'] = D['value_count'] / D['value_count'].sum()*100
+    D['ratio'] = D['ratio'].round()
     
-    most_common_pixel, count = pixel_counts.most_common(2)[1]
+    D = D.sort_values('value_count', ascending=False)
+    
+    common_colour = D.index[0]
+    ratio = str(D['ratio'].iloc[0])
+    
 
     st.markdown('## Most common colour is:')
-    st.write(str(most_common_pixel))
+    st.write(common_colour)
+    
+    st.markdown('## Colour occurs in this percentage of pixels:')
+    st.write(ratio)
     
